ac15104eeff6a5a8da38bb4aba0ab7bf0040b758
keyflight <olivier@andev.nl>
2019-08-15 19:49:01 +0700

    Initial commit

---
+++ tests-with-mathjs/test-14.test.js
@@ -0,0 +1,9 @@
+const mathjs = require('mathjs')
+
+describe('Memory jest mathjs', () => {
+
+    it('one plus one should equal two', () => {
+
+        expect(1 + 1).toEqual(2)
+    })
+})
---
+++ tests-without-mathjs/test-8.test.js
@@ -0,0 +1,8 @@
+
+describe('Memory jest mathjs', () => {
+
+    it('one plus one should equal two', () => {
+
+        expect(1 + 1).toEqual(2)
+    })
+})
